961f5c69b2979c30ef3dd51b8c6fd45aefc71937
Sigurður Kári Árnason <siggispes@Sigurdurs-MacBook-Pro.local>
2018-05-13 17:14:51 +0000

    Changes made in relations to icelandic implementation of bigfive personality test

--- config/index.js
+++ config/index.js
@@ -1,8 +1,8 @@
 'use strict'
 
 module.exports = {
-  saveUrl: process.env.BIGFIVE_GENERATOR_SAVE_URL || 'https://save.bigfive.maccyber.io',
-  evaluatorUrl: process.env.BIGFIVE_GENERATOR_EVALUATOR_URL || 'https://evaluator.bigfive.maccyber.io',
-  resultsUrl: process.env.BIGFIVE_GENERATOR_RESULTS_URL || 'https://results.bigfive.maccyber.io',
+  saveUrl: process.env.BIGFIVE_GENERATOR_SAVE_URL || 'https://micro-bigfive-save-iktuwppsmd.now.sh/',
+  evaluatorUrl: process.env.BIGFIVE_GENERATOR_EVALUATOR_URL || 'https://micro-bigfive-evaluator-fqeabqrqgj.now.sh/',
+  resultsUrl: process.env.BIGFIVE_GENERATOR_RESULTS_URL || 'https://micro-bigfive-results-tacnoyqysg.now.sh/',
   tokenKey: process.env.BIGFIVE_GENERATOR_TOKEN_KEY || 'Gibberish, jibberish, jibber-jabber and gobbledygook'
 }
--- index.js
+++ index.js
@@ -24,13 +24,16 @@ module.exports = async (req, res) => {
   const {query} = await parse(req.url, true)
   const data = req.method === 'POST' ? await json(req) : query
   if (req.method === 'POST') {
+    console.log(data);
     const scores = await axios.post(`${config.evaluatorUrl}`, data)
-    // console.log(scores.data)
+    console.log(scores.data)
     const save = await axios.post(`${config.saveUrl}`, scores.data)
-    // console.log(save.data)
+    console.log(save.data)
     result = save.data
   } else if (req.method === 'GET' && data.id) {
+    console.log(data);
     const answers = await axios.get(`${config.saveUrl}/?id=${data.id}`)
+    console.log(answers);
     const results = await axios.post(`${config.resultsUrl}`, answers.data[0])
     result = results.data
   } else {
